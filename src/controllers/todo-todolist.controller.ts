9a1baccad29e82b2c05588950e6fd3691d722687
KawanMN <kawan15marques@gmail.com>
2022-02-07 17:48:02 -0300

    Primeiro commit

---
+++ src/controllers/todo-todolist.controller.ts
@@ -0,0 +1,39 @@
+import {
+  repository
+} from '@loopback/repository';
+import {
+  get,
+  getModelSchemaRef,
+  param
+} from '@loopback/rest';
+import {
+  Todo,
+  Todolist
+} from '../models';
+import {TodoRepository} from '../repositories';
+
+export class TodoTodolistController {
+  constructor(
+    @repository(TodoRepository)
+    public todoRepository: TodoRepository,
+  ) { }
+
+  @get('/todos/{id}/todolist', {
+    responses: {
+      '200': {
+        description: 'Todolist belonging to Todo',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(Todolist)},
+          },
+        },
+      },
+    },
+  })
+
+  async getTodolist(
+    @param.path.number('id') id: typeof Todo.prototype.id,
+  ): Promise<Todolist> {
+    return this.todoRepository.todolist(id);
+  }
+}
---
+++ src/models/todo.model.ts
@@ -0,0 +1,57 @@
+import {belongsTo, Entity, model, property} from '@loopback/repository';
+import {Todolist} from './todolist.model';
+
+@model({
+  settings: {
+    foreignKeys: {
+      fk_todo_todoListId: {
+        name: 'fk_todo_todoListId',
+        entity: 'TodoList',
+        entityKey: 'id',
+        foreignKey: 'todoListId',
+      },
+    },
+  },
+})
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  desc?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  isComplete?: boolean;
+
+  @property({
+    type: 'number',
+  })
+  todoListId?: number;
+
+  @belongsTo(() => Todolist)
+  todolistId: number;
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
